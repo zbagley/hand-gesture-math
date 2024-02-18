873d88b93008a3b210cf43e7a0802aab479a688b
zbagley <zbagley@cgsinc.ca>
2024-02-18 12:02:54 -0600

    Added Webcam

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <p>{test}</p>
-        {children}
+        <Suspense fallback={<Loading />}>
+          <p>{test}</p>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,3 +1,171 @@
+'use client';
+
+import Script from 'next/script';
+import {
+  GestureRecognizer,
+  FilesetResolver,
+  DrawingUtils,
+} from '@mediapipe/tasks-vision';
+import Webcam from '@/components/webcam';
+import { useEffect, useRef, useState } from 'react';
+
 export default function Page() {
-  return <h1>Hello World - Test!</h1>;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [data, setData] = useState<MediaStream | null>(null);
+  const videoStream = (videoData: MediaStream | null) => {
+    setData(videoData);
+    console.log('got data: ', videoData);
+  };
+  useEffect(() => {
+    console.log('effect data: ', data);
+  }, [data]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      console.log(canvasRef);
+      const ctx = canvas.getContext('2d');
+    }
+  });
+  // let gestureRecognizer: GestureRecognizer;
+  // let enableWebcamButton: HTMLButtonElement;
+  // let webcamRunning: Boolean = false;
+  // const videoHeight = '360px';
+  // const videoWidth = '480px';
+
+  // const createGestureRecognizer = async () => {
+  //   const vision = await FilesetResolver.forVisionTasks(
+  //     'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm'
+  //   );
+  //   gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+  //     baseOptions: {
+  //       modelAssetPath:
+  //         'https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task',
+  //       delegate: 'GPU',
+  //     },
+  //     runningMode: 'VIDEO',
+  //   });
+  // };
+  // createGestureRecognizer();
+
+  // const video = document.getElementById('webcam');
+  // const canvasElement = document.getElementById('output_canvas');
+  // const canvasCtx = canvasElement.getContext('2d');
+  // const gestureOutput = document.getElementById('gesture_output');
+
+  // // Check if webcam access is supported.
+  // function hasGetUserMedia() {
+  //   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+  // }
+
+  // // If webcam supported, add event listener to button for when user
+  // // wants to activate it.
+  // if (hasGetUserMedia()) {
+  //   enableWebcamButton = document.getElementById('webcamButton');
+  //   enableWebcamButton.addEventListener('click', enableCam);
+  // } else {
+  //   console.warn('getUserMedia() is not supported by your browser');
+  // }
+
+  // // Enable the live webcam view and start detection.
+  // function enableCam(event) {
+  //   if (!gestureRecognizer) {
+  //     alert('Please wait for gestureRecognizer to load');
+  //     return;
+  //   }
+
+  //   if (webcamRunning === true) {
+  //     webcamRunning = false;
+  //     enableWebcamButton.innerText = 'ENABLE PREDICTIONS';
+  //   } else {
+  //     webcamRunning = true;
+  //     enableWebcamButton.innerText = 'DISABLE PREDICTIONS';
+  //   }
+
+  //   // getUsermedia parameters.
+  //   const constraints = {
+  //     video: true,
+  //   };
+
+  //   // Activate the webcam stream.
+  //   navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+  //     video.srcObject = stream;
+  //     video.addEventListener('loadeddata', predictWebcam);
+  //   });
+  // }
+
+  // let lastVideoTime = -1;
+  // let results = undefined;
+  // async function predictWebcam() {
+  //   const webcamElement = document.getElementById('webcam');
+  //   // Now let's start detecting the stream.
+  //   if (runningMode === 'IMAGE') {
+  //     runningMode = 'VIDEO';
+  //     await gestureRecognizer.setOptions({ runningMode: 'VIDEO' });
+  //   }
+  //   let nowInMs = Date.now();
+  //   if (video.currentTime !== lastVideoTime) {
+  //     lastVideoTime = video.currentTime;
+  //     results = gestureRecognizer.recognizeForVideo(video, nowInMs);
+  //   }
+
+  //   canvasCtx.save();
+  //   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  //   const drawingUtils = new DrawingUtils(canvasCtx);
+
+  //   canvasElement.style.height = videoHeight;
+  //   webcamElement.style.height = videoHeight;
+  //   canvasElement.style.width = videoWidth;
+  //   webcamElement.style.width = videoWidth;
+
+  //   if (results.landmarks) {
+  //     for (const landmarks of results.landmarks) {
+  //       drawingUtils.drawConnectors(
+  //         landmarks,
+  //         GestureRecognizer.HAND_CONNECTIONS,
+  //         {
+  //           color: '#00FF00',
+  //           lineWidth: 5,
+  //         }
+  //       );
+  //       drawingUtils.drawLandmarks(landmarks, {
+  //         color: '#FF0000',
+  //         lineWidth: 2,
+  //       });
+  //     }
+  //   }
+  //   canvasCtx.restore();
+  //   if (results.gestures.length > 0) {
+  //     gestureOutput.style.display = 'block';
+  //     gestureOutput.style.width = videoWidth;
+  //     const categoryName = results.gestures[0][0].categoryName;
+  //     const categoryScore = parseFloat(
+  //       results.gestures[0][0].score * 100
+  //     ).toFixed(2);
+  //     const handedness = results.handednesses[0][0].displayName;
+  //     gestureOutput.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore} %\n Handedness: ${handedness}`;
+  //   } else {
+  //     gestureOutput.style.display = 'none';
+  //   }
+  //   // Call this function again to keep predicting when the browser is ready.
+  //   if (webcamRunning === true) {
+  //     window.requestAnimationFrame(predictWebcam);
+  //   }
+  // }
+  return (
+    <section>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/vision_bundle.js"
+        crossOrigin="anonymous"
+      ></Script>
+      <h1>Hello World - Test!</h1>
+      <Webcam videoStream={videoStream}></Webcam>
+      <canvas
+        width="200px"
+        height="200px"
+        color="blue"
+        ref={canvasRef}
+      ></canvas>
+    </section>
+  );
 }
